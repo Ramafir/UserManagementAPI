d476fa2ec9fb52e309b422de80abf0bf3105dea0
Daniel Kołban <daniel.kolban@gmail.com>
2025-01-26 22:11:57 +0100

    removed unused files

--- src/tests/integration/probata/index.ts
+++
@@ -1,5 +0,0 @@
-export { defineTests } from "./defineTests";
-export { useTestAgent } from "./useTestAgent";
-export { loadTestsGroups } from "./loadTestsGroups";
-export { defineTestsGroup } from "./defineTestsGroup";
-export * from "./types";
