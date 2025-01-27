1a04004bb524edd56f58eac7393fcee5c973abc8
Daniel Kołban <daniel.kolban@gmail.com>
2025-01-27 08:39:36 +0100

    feature/user-store-controller

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,14 +3,18 @@ import { container } from 'tsyringe';
 
 import { ShowController } from '@controllers/User/ShowController';
 import { IndexController } from '@controllers/User/IndexController';
+import { StoreController } from '@controllers/User/StoreController';
 
 const router = express.Router();
 
-const indexController = container.resolve(IndexController);
 const showController = container.resolve(ShowController);
+const indexController = container.resolve(IndexController);
+const storeController = container.resolve(StoreController);
 
 router.get('/users', (req, res) => indexController.handle(req, res));
 
 router.get('/user/:id', (req, res) => showController.handle(req, res));
+
+router.post('/user', (req, res) => storeController.handle(req, res));
 
 export default router;
