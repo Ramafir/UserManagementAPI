acea34af4b95c863facf702af3695db0de4d3e91
Ramafir <57528769+Ramafir@users.noreply.github.com>
2025-01-27 09:17:16 +0100

    feature/update-user-controller (#6)
    Co-authored-by: Daniel Kołban <daniel.kolban@gmail.com>

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,17 +4,21 @@ import { container } from 'tsyringe';
 import { ShowController } from '@controllers/User/ShowController';
 import { IndexController } from '@controllers/User/IndexController';
 import { StoreController } from '@controllers/User/StoreController';
+import { UpdateController } from '@controllers/User/UpdateController';
 
 const router = express.Router();
 
 const showController = container.resolve(ShowController);
 const indexController = container.resolve(IndexController);
 const storeController = container.resolve(StoreController);
+const updateController = container.resolve(UpdateController);
 
 router.get('/users', (req, res) => indexController.handle(req, res));
 
 router.get('/user/:id', (req, res) => showController.handle(req, res));
 
 router.post('/user', (req, res) => storeController.handle(req, res));
+
+router.patch('/user/:id', (req, res) => updateController.handle(req, res));
 
 export default router;
